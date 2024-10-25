b95f8821dfac982dd5578bb6a498d10c28fd2e9f
Nanskip <nanskip.oof@gmail.com>
2024-10-25 15:12:47 +0300

    fixed scaling

--- modules/garden.lua
+++ modules/garden.lua
@@ -25,7 +25,7 @@ function garden.placeLine(self, number, type)
     local scale = 5
 
     line.shape.Scale = scale
-    line.shape.Position = Number3(number, 0, number%2) * scale
+    line.shape.Position = Number3(number, 0, number%2) * scale * 3
     line.shape:SetParent(World)
 
     self.lines[number] = line
