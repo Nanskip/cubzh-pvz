6b947ffd951ae1fcb849e14016c62b9d97360c45
Nanskip <nanskip.oof@gmail.com>
2024-10-25 20:21:54 +0300

    updated sunflower animation test

--- data/plants.lua
+++ data/plants.lua
@@ -8,7 +8,7 @@ local plants = {
             -- init function
         end,
         Tick = function(s)
-            s.object.Rotation.Y = s.object.Rotation.Y + 0.01
+            -- tick function
         end,
     },
     sunflower = {
@@ -17,10 +17,20 @@ local plants = {
         reload = 5,
         shape = "sunflower",
         Init = function(s)
-            -- init function
+            s.object.Rotation.Y = 1.2
+
+            s.body = s.object:GetChild(1) s.head = s.object:GetChild(2)
+            s.bodyrot = Rotation(body.LocalRotation.X, body.LocalRotation.Y, body.LocalRotation.Z)
+            s.headrot = Rotation(head.LocalRotation.X, head.LocalRotation.Y, head.LocalRotation.Z)
+            s.tick = 0
         end,
         Tick = function(s)
-            -- tick function
+            s.tick = s.tick + 1
+            if s.tick > 314 then
+                s.tick = 0
+            end
+            s.body.LocalRotation = Rotation(s.bodyrot.X, s.bodyrot.Y + math.sin(s.tick*0.01)*0.1, s.bodyrot.Z)
+            s.head.LocalRotation = Rotation(s.headrot.X, s.headrot.Y + math.cos(s.tick*0.01)*0.1, s.headrot.Z)
         end,
     },
 }
