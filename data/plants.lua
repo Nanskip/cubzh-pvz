c4d325726691d06054032b68819547c8060c2a32
Nanskip <nanskip.oof@gmail.com>
2024-10-25 20:35:47 +0300

    fixed head animation

--- data/plants.lua
+++ data/plants.lua
@@ -5,10 +5,18 @@ local plants = {
         reload = 10,
         shape = "peashooter",
         Init = function(s)
-            -- init function
+            s.body = s.object:GetChild(1) s.head = s.body:GetChild(1)
+            s.bodyrot = Rotation(s.body.LocalRotation.X, s.body.LocalRotation.Y, s.body.LocalRotation.Z)
+            s.headrot = Rotation(s.head.LocalRotation.X, s.head.LocalRotation.Y, s.head.LocalRotation.Z)
+            s.tick = 0
         end,
         Tick = function(s)
-            -- tick function
+            s.tick = s.tick + 1
+            if s.tick > 628 then
+                s.tick = 0
+            end
+            s.body.LocalRotation = Rotation(s.bodyrot.X + math.abs(math.sin(s.tick*0.05)*0.2), s.bodyrot.Y, s.bodyrot.Z)
+            s.head.LocalRotation = Rotation(s.headrot.X - math.abs(math.sin(s.tick*0.05)*0.2), s.headrot.Y, s.headrot.Z)
         end,
     },
     sunflower = {
@@ -26,11 +34,11 @@ local plants = {
         end,
         Tick = function(s)
             s.tick = s.tick + 1
-            if s.tick > 314 then
+            if s.tick > 628 then
                 s.tick = 0
             end
-            s.body.LocalRotation = Rotation(s.bodyrot.X + math.sin(s.tick*0.03)*0.2, s.bodyrot.Y, s.bodyrot.Z)
-            s.head.LocalRotation = Rotation(s.headrot.X - math.sin(s.tick*0.03)*0.2, s.headrot.Y, s.headrot.Z)
+            s.body.LocalRotation = Rotation(s.bodyrot.X + math.abs(math.sin(s.tick*0.05)*0.2), s.bodyrot.Y, s.bodyrot.Z)
+            s.head.LocalRotation = Rotation(s.headrot.X - math.abs(math.sin(s.tick*0.05)*0.2), s.headrot.Y, s.headrot.Z)
         end,
     },
 }
